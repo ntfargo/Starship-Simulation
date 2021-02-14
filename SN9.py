3d5eead80e6411bf71c688285f36c472719d0683
DrFargo <32229490+DrFargo@users.noreply.github.com>
2021-02-14 20:50:17 +0100

    Add files via upload

--- SN9.py
+++ SN9.py
@@ -1,8 +1,12 @@
 ## Author DrFargo
 ## Created: 2021-02-07
-## Latest update: 2021-02-08
+## Latest update: 2021-02-10
  
 from src.base import starshipSimulation as SS
 
+g = 9.81 #m/s2 Gravity of Earth
+mass_lox = 30e3 #Liquid oxygen
+Raptor = 3 # 1 Raptor Engine = 2.3 meganewton
+ 
 Sim = SS()
 Sim.Render('SN9')
--- src/base.py
+++ src/base.py
@@ -1,6 +1,6 @@
 ## Author DrFargo
 ## Created: 2021-02-07
-## Latest update: 2021-02-08
+## Latest update: 2021-02-12
 
 import matplotlib
 from mpl_toolkits.mplot3d import Axes3D
@@ -9,12 +9,26 @@ import scipy.linalg as la
 import matplotlib.pyplot as plt
 
 class starshipSimulation:
+    def parameters(self, g, lox, engines):
+        gravity = g
+        tlox = lox
+        rapteng = engines*2.3
+        m_fuel = 1.8
+        m_ox = 2.2
+        
+    #def DragForce(self, v):
+        
+
     def Render(self, filename):
         fig = plt.figure()
-        ax = fig.gca(projection='3d')
-        ax = self.XYZLabels(ax, 24000) 
+        ax = fig.gca(projection='3d') 
+        ax = self.XYZLabels(ax, 12000) 
         plt.savefig(filename + ".png")
         plt.show()
+        
+    def explode(self,t):
+        ax.text(0, 0, 0, "red", color='red')
+        return y[1]
         
     def XYZLabels(self, ax, Limit):
         TopAlt = np.max(Limit)
@@ -25,13 +39,3 @@ class starshipSimulation:
         ax.set_xlabel("Eastings")
         ax.set_ylabel("Northings")
         ax.set_zlabel("Altitude")
-
-class AxisData:
-    LabelDict = {
-        "Time":"Time (s)",
-        "AngleOfAttack":"Angle of Attack (rad)",
-        "Thrust":"Thrust (N)",
-        "X":"Eastings",
-        "Y":"Northings",
-        "Z":"Altitude",
-    }
